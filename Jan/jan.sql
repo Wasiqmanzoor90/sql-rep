1ba4190b8cc7ac093b8e881c52e9c11fd42fdcac
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-06-05 16:14:04 +0530

    l3

---
+++ Jan/jan.sql
@@ -0,0 +1,98 @@
+--single-line commet
+--This sql Day 1
+
+--Multi line commet--
+/*
+
+*/
+--DATA MANIPULATION LANGUAGE(DDL)--
+--Data defination Language(DDL)--
+--Transaction control language(TCL)--
+
+--Data type tell which type a data a particulat holds--
+
+--char    holds textual data
+--varchar  holds textual data
+-- int      numerical data
+--boolean   true/false 
+
+
+
+create table employ
+(
+id int,
+Name varchar(20)
+)
+
+
+create database mydb1
+
+use mydb
+drop database db2  --error
+drop database if exists db2 --handle error gracefully
+drop database mydb1  --here we are droping the databse
+
+
+/*Data defination lamguage(DDL):-DDL is used to define the structure 
+DDL Comands are as Follow:-
+1. CREATE    Create a table
+2. Alter     Make Change in existing Table or we cany it modifies the structure of table
+3. Truncate   it deletes internal content of table
+4. Drop       Drop means when delete whole table with data and its content
+*/
+
+drop table student
+
+Create table student
+(
+roll_no int,
+Name varchar(20),
+Section varchar(10)
+)
+
+truncate table student   --Here truncate only delete data not table 
+drop table student   --Here it delete whole table 
+
+Alter table student 
+add class varchar(30)
+select * from student  
+
+
+
+/*Data mainpulation Language(DML):- Dml deals with existing table or we can say existing instannce
+DML Comands
+1 . SELECT      it retreives data from database
+2. Insert       Adding data in particular table 
+3. Update       it updates existing data that are presented in table
+4. Delete       it deletes a particulat value inide a table 
+*/
+
+
+
+insert into student(roll_no,Name,Section, class)
+values(1,'wasiq','A','10th'),
+(2,'umer','B','10th')
+
+
+select * from student
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select * from employ
